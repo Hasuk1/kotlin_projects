4256cbbb181b92547aeeec12d5bb8381b6a9a6db
Perla Brunilda <perlabru@pr-p1.kzn.21-school.ru>
2024-01-25 20:56:05 +0300

    Part 1 done, but need add print

--- Bootcamp_03/src/exercise1/src/main/kotlin/Main.kt
+++ Bootcamp_03/src/exercise1/src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
-import kotlin.random.Random
-
 fun main() {
+  println("1. Adding elements")
   val revolverDrum = RevolverDrum<Int>()
   revolverDrum.addElement(54)
   revolverDrum.addElement(7)
@@ -8,25 +7,24 @@ fun main() {
   revolverDrum.addElement(56)
   revolverDrum.addElement(4)
   revolverDrum.addElement(3)
-  println("1. Adding elements")
   println("Structure: RevolverDrum<Int>")
   println("Objects: $revolverDrum")
   println("Pointer: ${revolverDrum.getPointer()}\n")
-  revolverDrum.scroll()
   println("2. Scroll")
+  revolverDrum.scroll()
   println("Structure: RevolverDrum<Int>")
   println("Objects: $revolverDrum")
   println("Pointer: ${revolverDrum.getPointer()}\n")
-  revolverDrum.deleteElement()
-  revolverDrum.deleteElement()
-  revolverDrum.deleteElement()
-  revolverDrum.deleteElement()
   println("3. Deletion")
+  revolverDrum.deleteElement()
+  revolverDrum.deleteElement()
+  revolverDrum.deleteElement()
+  revolverDrum.deleteElement()
   println("Structure: RevolverDrum<Int>")
   println("Objects: $revolverDrum")
   println("Pointer: ${revolverDrum.getPointer()}\n")
-  val collection = mutableListOf<Int?>(4, 6, 3, 22, 77, 43, 76, 5)
   println("4. Supply collection")
+  val collection = mutableListOf<Int?>(4, 6, 3, 22, 77, 43, 76, 5)
   println("Before:")
   println("Supply collection: $collection\n")
   println("Structure: RevolverDrum<Int>")
@@ -38,133 +36,39 @@ fun main() {
   println("Structure: RevolverDrum<Int>")
   println("Objects: $revolverDrum")
   println("Pointer: ${revolverDrum.getPointer()}\n")
-}
+  println("5. Extraction")
+  val extractedList = revolverDrum.removeAllElement()
+  println("The extracted list: $extractedList")
+  println("size: ${extractedList.size}\n")
+  println("Structure: RevolverDrum<Int>")
+  println("Objects: $revolverDrum")
+  println("Pointer: ${revolverDrum.getPointer()}")
+  println("size: ${revolverDrum.size()}\n")
+  println("6. Supply collection 2")
+  println("Before:")
+  println("Supply collection: $collection\n")
+  println("Structure: RevolverDrum<Int>")
+  println("Objects: $revolverDrum")
+  println("Pointer: ${revolverDrum.getPointer()}\n")
+  revolverDrum.addAllElement(collection)
+  println("After add operation performed:")
+  println("Supply collection: $collection\n")
+  println("Structure: RevolverDrum<Int>")
+  println("Objects: $revolverDrum")
+  println("Pointer: ${revolverDrum.getPointer()}\n")
+  println("7. Equals")
+  println("Structure: RevolverDrum<Int>")
+  println("Objects: $revolverDrum")
+  println("Pointer: ${revolverDrum.getPointer()}\n")
+  val revolverDrum2 = RevolverDrum<Int>()
+  revolverDrum2.addElement(77)
+  revolverDrum2.addElement(43)
+  revolverDrum2.addElement(76)
+  revolverDrum2.addElement(5)
+  println("Structure: RevolverDrum<Int>")
+  revolverDrum2.scroll()
+  println("Objects: $revolverDrum2")
+  println("Pointer: ${revolverDrum2.getPointer()}\n")
+  println(if (revolverDrum == revolverDrum2) "equals" else "not equals")
 
-class Index(private var index: Int) {
-  fun <T> findNextFree(list: MutableList<T>): Index? {
-    if (list[index] == null) return this
-    var i = index
-    var j = index
-    while (i in 0..6 && j in 0..6) {
-      if (i - 1 > 0 && list[--i] == null) {
-        index = i
-        return Index(index)
-      }
-      if (j + 1 < 6 && list[++j] == null) {
-        index = j
-        return Index(index)
-      }
-    }
-    return null
-  }
-
-  fun nextElement() {
-    if (index < 5) index++ else index = 0
-  }
-
-  fun randomElement() {
-    index = Random.nextInt(6)
-  }
-
-  fun toInt(): Int {
-    return index
-  }
-}
-
-class RevolverDrum<T>() {
-  private var list: MutableList<T?> = MutableList(6) { null }
-  private var index = Index(0)
-
-  fun getPointer(): String {
-    return "${list[index.toInt()]}"
-  }
-
-  private fun isFull():Boolean{
-    if (list.isEmpty()||list.size < 5) return false
-    for (i in 0 .. 5) {
-      if (list[i] == null) return false
-    }
-    return true
-  }
-
-  fun addElement(element: T): Boolean {
-    if (isFull())  return false
-    index = index.findNextFree(list) ?: return false
-    list[index.toInt()] = element
-    return true
-  }
-
-  fun addAllElement(elementList: MutableList<T?>): Boolean {
-    if (elementList.isEmpty()) return false
-    val iterator = elementList.iterator()
-    while (iterator.hasNext() && !isFull()) {
-      val element = iterator.next()
-      if (element != null) {
-        index.nextElement()
-        addElement(element)
-        iterator.remove()
-      }
-    }
-    return true
-  }
-
-  fun deleteElement(): Boolean {
-    if (list.isEmpty()) return false
-    if (list[index.toInt()] == null) return false
-    list[index.toInt()] = null
-    index.nextElement()
-    return true
-  }
-
-  fun removeElement(): T? {
-    if (list.isEmpty()) return null
-    if (list[index.toInt()] == null) return null
-    val element = list[index.toInt()]
-    index.nextElement()
-    return element
-  }
-
-  fun removeAllElement(): MutableList<T?> {
-    val removedElements = mutableListOf<T?>()
-    list.filterNotNull().forEach { _ ->
-      removedElements.add(removeElement())
-    }
-    return removedElements
-  }
-
-  fun scroll() {
-    index.randomElement()
-  }
-
-  fun size(): Int {
-    val sublist = list.subList(0, 6).filterNotNull()
-    return sublist.size
-  }
-
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (other !is RevolverDrum<*>) return false
-    val otherRevolver = other as? RevolverDrum<*> ?: return false
-
-    for (i in 0..5) {
-      val thisElement = list[(index.toInt() + i) % 6]
-      val otherElement = otherRevolver.list[(otherRevolver.index.toInt() + i) % 6]
-      if (thisElement != otherElement) return false
-    }
-
-    return true
-  }
-
-
-  override fun toString(): String {
-    val sublist = list.subList(index.toInt(), 6)
-    sublist.addAll(list.subList(0, index.toInt()))
-    return "$sublist"
-  }
-
-  override fun hashCode(): Int {
-    var result = list.hashCode()
-    result = 31 * result + index.hashCode()
-    return result
-  }
 }
--- Bootcamp_03/src/exercise1/src/main/kotlin/RevolverDrum.kt
+++ Bootcamp_03/src/exercise1/src/main/kotlin/RevolverDrum.kt
@@ -1,3 +1,106 @@
+class RevolverDrum<T>() {
+  private var list: MutableList<T?> = MutableList(6) { null }
+  private var index = Index(0)
+
+  fun getPointer(): String {
+    return "${list[index.toInt()]}"
+  }
+
+  fun isFull(): Boolean {
+    if (list.isEmpty() || list.size < 5) return false
+    for (i in 0..5) {
+      if (list[i] == null) return false
+    }
+    return true
+  }
+
+  fun isEmpty(): Boolean {
+    if (list.isEmpty()) return true
+    for (i in 0..5) {
+      if (list[i] != null) return false
+    }
+    return true
+  }
+
+  fun addElement(element: T?): Boolean {
+    if (isFull()) return false
+    index = index.findNextFree(list) ?: return false
+    list[index.toInt()] = element
+    return true
+  }
+
+  fun addAllElement(elementList: MutableList<T?>): Boolean {
+    if (elementList.isEmpty()) return false
+    val iterator = elementList.iterator()
+    while (iterator.hasNext() && !isFull()) {
+      val element = iterator.next()
+      if (element != null) {
+        index.nextElement()
+        addElement(element)
+        iterator.remove()
+      }
+    }
+    return true
+  }
+
+  fun deleteElement(): Boolean {
+    if (list.isEmpty()) return false
+    if (list[index.toInt()] == null) return false
+    list[index.toInt()] = null
+    index.nextElement()
+    return true
+  }
+
+  fun removeElement(): T? {
+    if (list.isEmpty()) return null
+    if (list[index.toInt()] == null) return null
+    val element = list[index.toInt()]
+    list[index.toInt()] = null
+    index.nextElement()
+    return element
+  }
+
+  fun removeAllElement(): MutableList<T?> {
+    val removedElements = mutableListOf<T?>()
+    for (i in 0..5) {
+      if (list[index.toInt()] != null) removedElements.add(removeElement())
+    }
+    return removedElements
+  }
+
+  fun scroll() {
+    index.randomElement()
+  }
+
+  fun size(): Int {
+    val sublist = list.subList(0, 6).filterNotNull()
+    return sublist.size
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is RevolverDrum<*>) return false
+    val otherRevolver = other as? RevolverDrum<*> ?: return false
+
+    for (i in 0..5) {
+      val thisElement = list[(index.toInt() + i) % 6]
+      val otherElement = otherRevolver.list[(otherRevolver.index.toInt() + i) % 6]
+      if (thisElement != otherElement) return false
+    }
+
+    return true
+  }
 
 
-//toString 1. print from index to end 2. print from begin until index
+  override fun toString(): String {
+    val sublist = list.subList(index.toInt(), 6)
+    sublist.addAll(list.subList(0, index.toInt()))
+    return "$sublist"
+  }
+
+  override fun hashCode(): Int {
+    var result = list.hashCode()
+    result = 31 * result + index.hashCode()
+    return result
+  }
+}
