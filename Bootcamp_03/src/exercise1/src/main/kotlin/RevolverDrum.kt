4289f42d4f10e02ca3820f4f2d6f49747ecfd282
Perla Brunilda <perlabru@pr-p1.kzn.21-school.ru>
2024-01-27 19:15:48 +0300

    Make class index provate

--- Bootcamp_03/src/exercise1/src/main/kotlin/RevolverDrum.kt
+++ Bootcamp_03/src/exercise1/src/main/kotlin/RevolverDrum.kt
@@ -1,4 +1,27 @@
+import kotlin.random.Random
+
 class RevolverDrum<T>() {
+  class Index(private var index: Int) {
+    fun <T> findNextFree(list: MutableList<T>): Index? {
+      for (i in 0..5) {
+        if (list[index] == null) return this
+        nextElement()
+      }
+      return null
+    }
+
+    fun nextElement() {
+      if (index < 5) index++ else index = 0
+    }
+
+    fun randomElement() {
+      index = Random.nextInt(6)
+    }
+
+    fun toInt(): Int {
+      return index
+    }
+  }
   private var list: MutableList<T?> = MutableList(6) { null }
   private var index = Index(0)
 
